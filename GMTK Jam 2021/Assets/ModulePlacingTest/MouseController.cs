39b4a5e2cb8401ce7c18d1f220386c691894a526
Wyatt Joyner <wyatt.joyner@practicalcode.us>
2021-06-12 15:10:26 -0400

    added extension methods

---
+++ GMTK Jam 2021/Assets/ModulePlacingTest/MouseController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+
+    public static MouseController instance;
+
+    public GameObject targetModule;
+
+    public bool mouseDown;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseDown = Input.GetMouseButton(0);
+    }
+}
