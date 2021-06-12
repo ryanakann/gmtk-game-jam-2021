bdc34f5d9b26c043bc39bf783165b75f3a31c2bf
SimonMarkWhittle <simonmarkwhittle@gmail.com>
2021-06-12 17:50:14 -0400

    renaming IonCannon components to prepare for multi-use with Missile

--- GMTK Jam 2021/Assets/Modules/ProjectileShooterModules/IonProjectile.cs
+++ GMTK Jam 2021/Assets/Modules/ProjectileShooterModules/IonProjectile.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class IonCannonProjectile : MonoBehaviour
+public class IonProjectile : MonoBehaviour
 {
 
     [SerializeField]
