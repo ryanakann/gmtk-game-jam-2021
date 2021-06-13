4bd62555fa504ce7d66017aeffec89f92357100c
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2021-06-12 21:00:41 -0400

    cache center of mass each update

--- GMTK Jam 2021/Assets/Scripts/MainModule.cs
+++ GMTK Jam 2021/Assets/Scripts/MainModule.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 [RequireComponent(typeof(Controller))]
 public class MainModule : Module {
     HashSet<Module> modules;//modules under my control
+    public Vector2 centerOfMass { get; private set; }
+
     protected override void Start() {
         base.Start();
         modules = new HashSet<Module>();
         mainModule = gameObject;
         AddModule(this);
+    }
+    protected override void Update() {
+        base.Update();
+        centerOfMass = getCenterOfMass();
     }
     public void AddModule(Module m) {
         modules.Add(m);
